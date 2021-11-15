a4b0fc2ae57bad99ea492989cd9f554678127f9c
Juhanir <juhani.riisio@gmail.com>
2021-11-15 20:39:26 +0200

    add coverage and related tasks

--- tasks.py
+++ tasks.py
@@ -5,6 +5,17 @@ from invoke import task
 def test(ctx):
     ctx.run('pytest ./src')
 
+
 @task
 def start(ctx):
     ctx.run('python3 ./src/index.py')
+
+
+@task
+def coverage(ctx):
+    ctx.run('coverage run --branch -m pytest src')
+
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run('coverage html')
