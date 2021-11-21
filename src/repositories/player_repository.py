8650ee4c00404f3e744a7196b3417b9ffd840d77
Juhanir <juhani.riisio@gmail.com>
2021-11-21 10:50:27 +0200

    player_repo refactor

--- src/repositories/player_repository.py
+++ src/repositories/player_repository.py
@@ -6,14 +6,13 @@ class PlayerRepository:
     def __init__(self, connection: 'Connection'):
         self._connection = connection
 
-    def add_player(self, playername) -> bool:
-        print('adding player', playername)
+    def add_player(self, player: 'Player') -> bool:
         self._connection.cursor().execute(
-            "INSERT INTO Players(playername) VALUES (?);", (playername,))
+            "INSERT INTO Players(playername) VALUES (?);", (player.playername,))
         self._connection.commit()
         return True
 
-    def find_player(self, playername) -> 'Player':
+    def find_player_by_name(self, playername) -> 'Player':
         cursor = self._connection.cursor().execute(
             "SELECT * FROM Players WHERE playername = (?);", (playername,))
         row = cursor.fetchone()
@@ -21,13 +20,13 @@ class PlayerRepository:
             return None
         return Player(row['id'], row['playername'], row['last_login'])
 
-    def set_last_login(self, playername, timestamp) -> bool:
+    def set_last_login(self, player: 'Player') -> bool:
         cursor = self._connection.cursor().execute(
-            "SELECT * FROM Players WHERE playername = (?);", (playername,))
+            "SELECT * FROM Players WHERE playername = (?);", (player.playername,))
         row = cursor.fetchone()
         if row is None:
             return False
         cursor.execute("UPDATE Players SET last_login = (?) WHERE id = (?)",
-                       (timestamp, row['id']))
+                       (player.last_login, row['id']))
         self._connection.commit()
         return True
--- src/tests/repositories/player_repository_test.py
+++ src/tests/repositories/player_repository_test.py
@@ -8,6 +8,7 @@ from db_connection import get_db_connection
 class TestUser(unittest.TestCase):
     def setUp(self):
         self.connection = get_db_connection()
+        self.test_player = Player(playername="test_player")
 
     def clear_db(self):
         self.connection.cursor().execute("DELETE FROM Players;")
@@ -16,13 +17,13 @@ class TestUser(unittest.TestCase):
 
     def test_add_player_returns_true_when_no_error(self):
         repo = PlayerRepository(self.connection)
-        res = repo.add_player("test_player")
+        res = repo.add_player(self.test_player)
         self.assertTrue(res)
         self.clear_db()
 
     def test_add_player_adds_one_player_to_db(self):
         repo = PlayerRepository(self.connection)
-        repo.add_player("test_player")
+        repo.add_player(self.test_player)
         cursor = self.connection.cursor().execute("SELECT COUNT(*) FROM Players;")
         res = cursor.fetchone()
         self.assertEqual(res[0], 1)
@@ -30,15 +31,15 @@ class TestUser(unittest.TestCase):
 
     def test_find_player_returns_player(self):
         repo = PlayerRepository(self.connection)
-        repo.add_player("test_player")
-        player = repo.find_player("test_player")
+        repo.add_player(self.test_player)
+        player = repo.find_player_by_name("test_player")
         self.assertIsInstance(player, Player)
         self.clear_db()
 
     def test_returned_player_has_correct_instance_properties(self):
         repo = PlayerRepository(self.connection)
-        repo.add_player("test_player")
-        player = repo.find_player("test_player")
+        repo.add_player(self.test_player)
+        player = repo.find_player_by_name("test_player")
         self.assertEqual(player.id, 1)
         self.assertEqual(player.playername, "test_player")
         self.assertEqual(player.last_login, None)
@@ -46,33 +47,37 @@ class TestUser(unittest.TestCase):
 
     def test_find_player_returns_none_when_not_found(self):
         repo = PlayerRepository(self.connection)
-        repo.add_player("test_player")
-        player = repo.find_player("foo_bar")
+        repo.add_player(self.test_player)
+        player = repo.find_player_by_name("foo_bar")
         self.assertEqual(player, None)
         self.clear_db()
 
     def test_set_last_login_returns_true(self):
         repo = PlayerRepository(self.connection)
-        repo.add_player("test_player")
-        res = repo.set_last_login("test_player", datetime.now().timestamp())
+        repo.add_player(self.test_player)
+        self.test_player.set_login_time(datetime.now().timestamp())
+        res = repo.set_last_login(self.test_player)
         self.assertTrue(res)
         self.clear_db()
 
     def test_set_last_login_returns_false_when_no_player_found(self):
         repo = PlayerRepository(self.connection)
-        repo.add_player("test_player")
-        res = repo.set_last_login("foo_player", datetime.now().timestamp())
+        repo.add_player(self.test_player)
+        not_added = Player("new_player")
+        not_added.set_login_time(datetime.now().timestamp())
+        res = repo.set_last_login(not_added)
         self.assertFalse(res)
         self.clear_db()
 
     def test_set_last_login_sets_timestamp(self):
         repo = PlayerRepository(self.connection)
-        repo.add_player("test_player")
+        repo.add_player(self.test_player)
         cursor = self.connection.cursor().execute(
             "SELECT last_login FROM Players WHERE playername = 'test_player';")
         res = cursor.fetchone()
         self.assertIsNone(res[0])
-        repo.set_last_login("test_player", datetime.now().timestamp())
+        self.test_player.set_login_time(datetime.now().timestamp())
+        repo.set_last_login(self.test_player)
         cursor = self.connection.cursor().execute(
             "SELECT last_login FROM Players WHERE playername = 'test_player';")
         res = cursor.fetchone()
