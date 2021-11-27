0ed9c927690aa1b356e5253953c380997fe829eb
Juhanir <juhani.riisio@gmail.com>
2021-11-27 19:05:17 +0200

    Pac cannot move through walls

--- src/game/level.py
+++ src/game/level.py
@@ -15,7 +15,7 @@ class Level:
         self._create_level(level_map)
 
     def _create_level(self, level_map):
-        for y in range(len(level_map)): # pylint: disable=consider-using-enumerate
+        for y in range(len(level_map)):  # pylint: disable=consider-using-enumerate
             for x in range(len(level_map[0])):
                 cell = level_map[y][x]
                 if cell == 2:
@@ -27,12 +27,24 @@ class Level:
 
         self.sprites.add(self.pac, self.walls, self.nuggets)
 
+    def _check_collision(self, sprites):
+        return len(pygame.sprite.spritecollide(self.pac, sprites, False))
+
+
     def move_pac(self, direction):
+        d_x, d_y = 0, 0
         if direction == pygame.K_LEFT:
             self.pac.rect.move_ip(-CELL_SIZE, 0)
+            d_x, d_y = CELL_SIZE, 0
         elif direction == pygame.K_RIGHT:
             self.pac.rect.move_ip(CELL_SIZE, 0)
+            d_x, d_y = -CELL_SIZE, 0
         elif direction == pygame.K_UP:
             self.pac.rect.move_ip(0, -CELL_SIZE)
+            d_x, d_y = 0, CELL_SIZE
         elif direction == pygame.K_DOWN:
             self.pac.rect.move_ip(0, CELL_SIZE)
+            d_x, d_y = 0, -CELL_SIZE
+
+        if self._check_collision(self.walls):
+            self.pac.rect.move_ip(d_x, d_y)
